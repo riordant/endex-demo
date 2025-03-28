fb66434f16e798f6e1831a887166fbb54676487b
architect-dev <halftonedev@gmail.com>
2025-03-28 15:50:34 +0100

    Let there be light + cofhe-hardhat-plugin

---
+++ hardhat.config.ts
@@ -0,0 +1,15 @@
+import { HardhatUserConfig } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox'
+import '@fhenixprotocol/cofhe-hardhat-plugin'
+
+const config: HardhatUserConfig = {
+	solidity: {
+		version: '0.8.25',
+		settings: {
+			evmVersion: 'cancun',
+		},
+	},
+	defaultNetwork: 'localcofhe',
+}
+
+export default config
